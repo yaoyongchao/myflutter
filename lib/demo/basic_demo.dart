a18e738ad89cb4c8181fd0dfb35f46d13aa08695
yaoyongchao <yaoyongchao3g@163.com>
2019-05-15 19:06:30 +0800

    1、装饰盒子，

---
+++ lib/demo/basic_demo.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+class BasicDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 26.0,
+  );
+  final String _author = '李白';
+  final String _title = '将近酒';
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color:  Colors.grey[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.pool,size: 32.0,color: Colors.white),
+//            color: Color.fromRGBO(3, 54, 255, 1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+
+//  Text(
+//  '《 $_title 》—— $_author。君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。',
+//  textAlign: TextAlign.left,
+//  style: _textStyle,
+//  maxLines: 3,
+//  overflow: TextOverflow.ellipsis,
+//  );
+
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RichText(
+      text: TextSpan(
+          text: 'ninghao',
+          style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 34.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w100,
+          ),
+          children: [
+            TextSpan(
+                text: '.net',
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: Colors.grey,
+                )
+            )
+          ]
+      ),
+
+    );
+  }
+
+}
--- lib/my.dart
+++ lib/my.dart
@@ -4,6 +4,7 @@ import 'demo/ListViewDemo.dart';
 import 'demo/hello_demo.dart';
 import 'demo/DrawDemo.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/basic_demo.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -76,7 +77,8 @@ class Home extends StatelessWidget {
             children: <Widget>[
 //              Icon(Icons.local_florist,size: 128.0,color: Colors.black12,),
               ListViewDemo(),
-              Icon(Icons.change_history,size: 128.0,color: Colors.black12,),
+//              Icon(Icons.change_history,size: 128.0,color: Colors.black12,),
+              BasicDemo(),
               Icon(Icons.directions_bike,size: 128.0,color: Colors.black12,),
             ]),
         drawer: DrawDemo(),
